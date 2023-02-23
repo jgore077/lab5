6854bd72d69610bb7967fab26bdd77e5ade51b22
james.gore <james.gore@gmail.com>
2023-02-23 18:28:23 -0500

    Added a method to add passengers in a sorted order

--- Airline.java
+++ Airline.java
@@ -1,10 +1,57 @@
 import java.util.*;
 
 public class Airline {
-    ArrayList<Passenger> passengers;
-    
+
+    Passenger[] passengers;
+    int first=10;
+    int buisness=10;
+    int economy =20;
     public Airline(){
-    passengers =new ArrayList<Passenger>();
+    passengers =new Passenger[30];
+    }
+
+    public void addPassenger(Passenger person){
+        switch(person.seatClass){
+            case "F":
+                passengers[person.seatNumber-1] = person;
+                first--;
+                break;
+            case "B":
+                passengers[10+person.seatNumber-1] = person;
+                buisness--;
+                break;
+            default:
+                passengers[20+person.seatNumber-1] = person;
+                economy--;
+            }
+    }
+    public void checkInAtGate(){
+
+    }
+
+    /*
+     * I reccomend you start from index 0 then go till you find a null
+     *  then jump to index 10 and then once you find a null in the 10s
+     * jump to the 20s
+     */
+    public void boardPlane(ArrayList<Passenger> passengersList){
+        /*Write your code here */
+
+    }
+    //for anusha for example this method would be called readPassengersFromFile(delta.txt)
+    public ArrayList<Passenger> readPassengersFromFile(String filename){
+        ArrayList<Passenger> passengerList = new ArrayList<Passenger>();
+        /*Write your code here */
+        return passengerList;
+    }
+
+
+
+    /*
+     * Designing testing will be wills job
+     */
+    public static void  main(String[] args) {
+
 
     }
 }
