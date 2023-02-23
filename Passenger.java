c9658c8431dc9dfa2e409b5620ce3b3805a4a4b1
james.gore <james.gore@gmail.com>
2023-02-23 15:49:12 -0500

    wrote constructor

--- Passenger.java
+++ Passenger.java
@@ -1,3 +1,15 @@
  public class Passenger {
-    
+
+    protected String seatClass;
+    protected int seatNumber;
+    protected String name;
+   
+    public Passenger(String passengerName, String seat, int number){
+        seatClass=seat;
+        name=passengerName;
+        seatNumber =number;
+    }
+    public static void  main(String[] args) {
+        
+    }
 }
