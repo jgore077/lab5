f05feecc4ae6b2075b45fb4dccbaf9af579e4c75
anu-sha19 <126127739+anu-sha19@users.noreply.github.com>
2023-02-28 10:32:04 -0500

    Update Passenger.java

--- Passenger.java
+++ Passenger.java
@@ -3,11 +3,13 @@
     protected String seatClass;
     protected int seatNumber;
     protected String name;
+    protected boolean checkedIn;
    
     public Passenger(String passengerName, String seat, int number){
         seatClass=seat;
         name=passengerName;
         seatNumber =number;
+        checkedIn = false;
     }
     public static void  main(String[] args) {
         
