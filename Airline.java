757a33693352569e594113bbde43c5ba925dcc43
anu-sha19 <126127739+anu-sha19@users.noreply.github.com>
2023-02-28 10:30:24 -0500

    Update Airline.java

--- Airline.java
+++ Airline.java
@@ -26,7 +26,30 @@ public class Airline {
             }
     }
     public void checkInAtGate(Airline plane,String passengerName){
-
+        Passenger passenger = null;
+		// Find the passenger in the booked passengers list
+		for (Passenger pass : plane.passengers) {
+			if (pass != null && pass.name.equalsIgnoreCase(passengerName)) {
+				passenger = pass;
+				break;
+			}
+		}
+		if (passenger == null) {
+			System.out.println("Passenger not found!");
+			return;
+		}
+		// Check if the passenger has already checked in
+		if (passenger.checkedIn) {
+			System.out.println(passenger.name + " has already checked in!");
+			return;
+		}
+		// Mark the passenger as checked in
+		passenger.checkedIn = true;
+		// Print the boarding pass
+		System.out.println("Boarding pass:");
+		System.out.println("Passenger name: " + passenger.name);
+		System.out.println("Class: " + passenger.seatClass);
+		System.out.println("Seat number: " + passenger.seatNumber);
     }
 
     /*
@@ -39,9 +62,29 @@ public class Airline {
     }
     //for anusha for example this method would be called readPassengersFromFile(delta.txt)
     public Passenger[] readPassengersFromFile(String filename){
-        Passenger[] passengerList = new Passenger[30];
-        /*Write your code here */
-        return passengerList;
+        // Creating an array to store Passenger objects
+		Passenger[] passengerList = new Passenger[30];
+
+		// Scanner object to read from the file
+		Scanner scanner = new Scanner(new File("Delta.txt"));
+
+		//// Reading the current line and spliting it into an array of Strings
+		int index = 0;
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			String[] data = line.split(",");
+
+			// Taking out the relevant data
+			String name = data[0];
+			String seatClass = data[1];
+			int seatNumber = Integer.parseInt(data[2]);//parse.Int converts the string to an int primitive data type
+
+
+			//// Creating a new Passenger object using the data above and adding it to the passengerList array
+			passengerList[index] = new Passenger(name, seatClass, seatNumber);
+			index++;
+		}
+		return passengerList;
     }
 
 
